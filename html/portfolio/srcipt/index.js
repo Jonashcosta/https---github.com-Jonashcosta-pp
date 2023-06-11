fc03ff16a8af724d8a6bd0a69278c51a4f13fc8b
Jonashcosta <jonashonoratocosta@gmail.com>
2023-06-11 20:44:27 -0300

    adicionado languages usadas

--- html/portfolio/srcipt/index.js
+++ html/portfolio/srcipt/index.js
@@ -42,6 +42,11 @@ async function startrepos(reposURL) {
   for (const key in repos) {
     if (Object.hasOwnProperty.call(repos, key)) {
       const element = repos[key];
+      const languages = await fetch(element.languages_url, {
+        mode: 'cors',
+      }).then((res) => {
+        return res.json();
+      });
 
       // Buscar elemento pai
       let elemento_pai = document.getElementById('cards');
@@ -52,10 +57,41 @@ async function startrepos(reposURL) {
       let tagA = document.createElement('a');
       let tagH2 = document.createElement('h2');
       let tagH3 = document.createElement('h3');
+      let tagUL = document.createElement('ul');
+
+      let totalLanguage = 0;
+      for (const key in languages) {
+        if (Object.hasOwnProperty.call(languages, key)) {
+          const language = languages[key];
+          totalLanguage = totalLanguage + language;
+        }
+      }
+      for (const key in languages) {
+        if (Object.hasOwnProperty.call(languages, key)) {
+          const language = languages[key];
+          let result = (language * 100) / totalLanguage;
+          let tagLI = document.createElement('li');
+          tagLI.appendChild(
+            document.createTextNode(key + ' ' + result.toFixed(3) + '%')
+          );
+          tagUL.appendChild(tagLI);
+        }
+      }
+
+      //   // Buscar elemento pai
+      //   let elemento_pai = document.getElementById('cards');
+
+      //   // Criar elemento
+
+      //   let tagDiv = document.createElement('div');
+      //   let tagA = document.createElement('a');
+      //   let tagH2 = document.createElement('h2');
+      //   let tagH3 = document.createElement('h3');
 
       // inserir class
       tagDiv.classList.add('card');
       tagA.classList.add('link');
+      tagUL.classList.add('ul-lenguages');
 
       tagA.href = element.html_url;
       tagA.target = '_blank';
@@ -68,6 +104,7 @@ async function startrepos(reposURL) {
 
       tagDiv.appendChild(tagA);
       tagDiv.appendChild(tagH3);
+      tagDiv.appendChild(tagUL);
       elemento_pai.appendChild(tagDiv);
     }
   }
